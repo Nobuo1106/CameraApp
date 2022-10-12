31415c351510670f7a5eab01c4fd9dc9db2eea8c
initial commit <>
2022-10-12 09:36:41 +0900

    initial commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by apple on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
